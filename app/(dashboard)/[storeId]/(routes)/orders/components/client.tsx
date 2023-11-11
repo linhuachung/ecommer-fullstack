8aae87d33422622b2eb48f965bb9f67d37c8c048
Hua Chung <chunglh@bap.jp>
2023-11-11 19:26:57 +0700

    finally new branch

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,21 +1,23 @@
 "use client";
 
-import {Heading} from "@/components/ui/heading";
-import React from "react";
-import {Separator} from "@/components/ui/separator";
-import {DataTable} from "@/components/ui/data-table";
-import {columns, OrderColumn} from "@/app/(dashboard)/[storeId]/(routes)/orders/components/columns";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
-interface OderClientProps {
-    data: OrderColumn[]
+import { columns, OrderColumn } from "./columns";
+
+interface OrderClientProps {
+  data: OrderColumn[];
 }
 
-export const OrderClient: React.FC<OderClientProps> = ({data}) => {
-    return (
-        <>
-            <Heading title={`Orders (${data.length})`} description="Manage orders for your store"/>
-            <Separator/>
-            <DataTable columns={columns} data={data} searchKey={"products"}/>
-        </>
-    );
+export const OrderClient: React.FC<OrderClientProps> = ({
+  data
+}) => {
+  return (
+    <>
+      <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
+      <Separator />
+      <DataTable searchKey="products" columns={columns} data={data} />
+    </>
+  );
 };
--- components/ui/api-list.tsx
+++ components/ui/api-list.tsx
@@ -1,25 +1,30 @@
-import React from "react";
-import {useParams} from "next/navigation";
-import {useOrigin} from "@/hook/use-origin";
-import {ApiAlert} from "@/components/ui/api-alert";
+"use client";
+
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
+import { useParams } from "next/navigation";
 
 interface ApiListProps {
-    entityName: string
-    entityIdName: string
+  entityName: string;
+  entityIdName: string;
 }
-export const ApiList:React.FC<ApiListProps> = ({entityName,entityIdName}) => {
 
-    const params = useParams()
-    const origin = useOrigin()
+export const ApiList: React.FC<ApiListProps> = ({
+  entityName,
+  entityIdName,
+}) => {
+  const params = useParams();
+  const origin = useOrigin();
 
-    const baseUrl = `${origin}/api/${params.storeId}`
-    return (
-        <>
-            <ApiAlert title={"GET"} description={`${baseUrl}/${entityName}`} variant={"public"}/>
-            <ApiAlert title={"GET"} description={`${baseUrl}/${entityName}/{${entityIdName}}`} variant={"public"}/>
-            <ApiAlert title={"POST"} description={`${baseUrl}/${entityName}`} variant={"admin"}/>
-            <ApiAlert title={"PATCH"} description={`${baseUrl}/${entityName}/{${entityIdName}}`} variant={"admin"}/>
-            <ApiAlert title={"DELETE"} description={`${baseUrl}/${entityName}/{${entityIdName}}`} variant={"admin"}/>
-        </>
-    )
-}
+  const baseUrl = `${origin}/api/${params.storeId}`;
+
+  return (
+    <>
+      <ApiAlert title="GET" variant="public" description={`${baseUrl}/${entityName}`} />
+      <ApiAlert title="GET" variant="public" description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
+      <ApiAlert title="POST" variant="admin" description={`${baseUrl}/${entityName}`} />
+      <ApiAlert title="PATCH" variant="admin" description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
+      <ApiAlert title="DELETE" variant="admin" description={`${baseUrl}/${entityName}/{${entityIdName}}`} />
+    </>
+  );
+};
